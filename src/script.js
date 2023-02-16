7ec0234c10225c221950a6827a88bdff4c9f7da8
netizen112 <netizen112@gmail.com>
2023-02-16 10:06:31 +0200

    add functionality for resizable grid

--- src/script.js
+++ src/script.js
@@ -1,13 +1,42 @@
-//Making the grid cells
+//Global variables
+const canvas = document.querySelector(".canvas");
 
+//Handles color change when mouse goes over a grid cell
+function colorChange(e){
+    e.target.style.backgroundColor = "lightblue";
+}
+
+//Adds gridSize * gridSize number of cells (divs) to the canvas div
 function addGridCells(gridSize) {
     let numCells = gridSize * gridSize;
-    const canvas = document.querySelector(".canvas");
 
     for (let i = 0; i < numCells; i++) {
         const cell = document.createElement("div");
         cell.classList.add(`cell`); //cell${i + 1}
         canvas.appendChild(cell);
+
+        cell.addEventListener('mouseover', colorChange);
+    }
+}
+
+let inputGridSize = 0;
+
+function getInput(){
+    inputGridSize = prompt("Enter Grid Size (max 100)");
+    if (inputGridSize > 0 && inputGridSize <= 100){
+        canvas.replaceChildren();
+        addGridCells(inputGridSize);
+
+        canvas.style = `
+        display: grid;
+        height: 500px;
+        width: 500px;
+        border: 2px solid red;
+        grid-template-columns: repeat(${inputGridSize}, 1fr);
+        grid-template-rows: repeat(${inputGridSize}, 1fr);
+        `
+    } else {
+        alert("Invalid Grid Size");
     }
 }
 
